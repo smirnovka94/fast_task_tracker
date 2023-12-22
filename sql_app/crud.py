fe9c65eee95319f93bc12b62d511770f1b0c04d8
lekseich8 <lekseich8@gmail.com>
2023-12-22 22:46:01 +0300

    Эндпоинт Важных задач

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -1,9 +1,6 @@
-from pydantic import BaseModel
-from sqlalchemy import Null
-
 from sqlalchemy.orm import Session
 from sql_app.models import Employee, Task
-from sql_app.schemas import EmployeeCreate, EmployeeUpdate, TaskCreate, TaskUpdate, EmployeeBusy
+from sql_app.schemas import EmployeeCreate, EmployeeUpdate, TaskCreate, TaskUpdate
 
 
 # Получение списка  занятых сотрудников
@@ -31,50 +28,20 @@ def get_busy_employees(db: Session, status="active"):
 
     return sort_employees
 
-def get_free_employees(task, employees):
-    # Список закрепленных задач
-
-    # employees = db.query(Employee).all()
-    free_employees = []
-    free_id_employees = []
-    employee_tasts = {}
-
-    for employee in employees:
-        # Ищем занятые задачисотрудниками
-        tasks = db.query(Task).filter(Task.employee == employee.id).count()
-        all_tasks = db.query(Task).all()
-        employee.tasks = tasks
-        employee_tasts[employee] = tasks
-
-    employee_sorted = sorted(employee_tasts.items(), key=lambda x: x[1])
-
-    results = []
-    for keys, values in employee_sorted:
-        print(f"{keys.first_name} {values == 0}")
-        if values == 0:
-            results.append(keys)
-        if results == []:
-            results = employee_sorted[0]
-    return [{"ФИО": f"{result.first_name, result.second_name}"} for result in results]
-
-
-    #[{"first_name": employee.first_name, "second_name": employee.second_name} for employee in employees]
-
 
 def get_important_tasks(db: Session):
     employees = db.query(Employee).all()
-    employee_tasks = {}
+
     all_tasks = db.query(Task).all()
 
     sub_tasks = db.query(Task).filter(Task.related_task != None).all()
 
     id_parrent_tasks = []
-    id_employee_with_parrent_tasks=[]
+    id_employee_with_parrent_tasks = []
 
     for sub_task in sub_tasks:
         # Получаем id родительской задачи
         id_parrent_tasks.append(sub_task.related_task)
-
 
     for task in all_tasks:
         if (task.id in id_parrent_tasks) and (task.employee != None):
@@ -88,21 +55,15 @@ def get_important_tasks(db: Session):
         tasks = db.query(Task).filter(Task.employee == employee.id).all()
         employee.tasks = len(tasks)
         # Находим наименее занятого
-        if int(employee.tasks)<count_tasks:
+        if int(employee.tasks) < count_tasks:
             count_tasks = len(tasks)
             free_employee = employee
 
         # Ищем незагруженного сотрудника с родительскими задачами
         if employee.id in id_employee_with_parrent_tasks:
-            if int(employee.tasks)<count_tasks_p:
+            if int(employee.tasks) < count_tasks_p:
                 count_tasks_p = employee.tasks
                 free_employee_with_parrent_tasks = employee
-
-
-        employee.list_id_tasks = [task.id for task in tasks]
-        employee_tasks[employee] = len(tasks)
-    # сортируем employee по возрастанию занятости
-    employee_sorted = sorted(employee_tasks.items(), key=lambda x: x[1])
 
     # Ищем наиболее подходящего кандидата
     if int(free_employee_with_parrent_tasks.tasks) <= int(free_employee.tasks) + 2:
@@ -110,31 +71,19 @@ def get_important_tasks(db: Session):
     else:
         ideal_employee = free_employee
 
-    # print(f"id_employee_with_parrent_tasks {id_employee_with_parrent_tasks}")
-    #
-    # print(f"Свободный сотрудник {free_employee.first_name}-{free_employee.tasks}")
-    # print(f"Малозагруженный родительский сотрудник {free_employee_with_parrent_tasks.first_name}-{free_employee_with_parrent_tasks.tasks}")
-
-
-    # Получаем зависимые задачи, которые уже в работе
-    sub_tasks = db.query(Task).filter((Task.related_task != None) & (Task.status == "working")).all()
-    # id_parrent_tasks
     # Получаем активные задачи
-    parrent_tasks = []
-
+    important_tasks = []
     tasks_active = db.query(Task).filter(Task.status == "active").all()
     for task in tasks_active:
-        # db.query(Employee).filter(Employee.id == employee.id).count()
-
         if task.id in id_parrent_tasks:
             if ideal_employee.patronymic_name == None:
                 task.employees = [ideal_employee.first_name, ideal_employee.second_name]
             else:
                 task.employees = [ideal_employee.first_name, ideal_employee.second_name, ideal_employee.patronymic_name]
-            filter_task = {"Важная задача": task.name, "Срок": task.time_limit_hours,"ФИО сотрудника": task.employees}
-            parrent_tasks.append(filter_task)
+            filter_task = {"Важная задача": task.name, "Срок": task.time_limit_hours, "ФИО сотрудника": task.employees}
+            important_tasks.append(filter_task)
 
-    return parrent_tasks
+    return important_tasks
 
 
 # Создание сотрудника
